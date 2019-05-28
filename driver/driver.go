02d7d6fae6af7ac52167599f4953c914f3881d05
JamesAndresCM <jaimecanalesmarin@gmail.com>
2019-05-27 22:46:00 -0400

    change type

--- driver/driver.go
+++ driver/driver.go
@@ -13,11 +13,13 @@ type DB struct {
 
 var dbConn = &DB{}
 
-func ConnectPSQL(host, port, user, password, dbname string) (*DB, error){
+func ConnectPSQL(host, port int, user, password, dbname string) (*DB, error){
   psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
     "password=%s dbname=%s sslmode=disable",
     host, port, user, password, dbname)
   db, err := sql.Open("postgres", psqlInfo)
+  
+  
   if err != nil {
     panic(err)
   }
