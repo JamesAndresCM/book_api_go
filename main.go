145906a30851c958a0c58249a4212e8ca6b003d8
JamesAndresCM <jaimecanalesmarin@gmail.com>
2019-05-29 00:25:38 -0400

    first refactors

--- main.go
+++ main.go
@@ -1,27 +1,23 @@
 package main
 
 import (
-	"encoding/json"
 	"os"
-	"fmt"
   "github.com/gorilla/mux"
   "log"
   "net/http"
   "strconv"
-
+  "github.com/JamesAndresCM/book_api_go/controllers"
 	"github.com/subosito/gotenv"
-	"github.com/JamesACM/book_api_go/driver"
+	"github.com/JamesAndresCM/book_api_go/driver"
 )
 
 
-type Book struct{
-  ID int `json:id`
-  Title string `json:title`
-  Author string `json:author`
-  Year int `json:year`
+
+func logFatal(err error){
+  if err != nil {
+    log.Fatal(err)
+  }
 }
-
-
 
 func main(){
   gotenv.Load()
@@ -29,41 +25,19 @@ func main(){
   dbName := os.Getenv("DB_NAME")
   dbPass := os.Getenv("DB_PASS")
   dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
-	
-	connection, err := driver.ConnectSQL(dbHost, dbPort, "root", dbPass, dbName)
-
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(-1)
-	}
+  dbPort := os.Getenv("DB_PORT")
+  
+  portdb, _ := strconv.Atoi(dbPort)
+	db, err := driver.ConnectPSQL(dbHost, "snake", dbPass, dbName, portdb)
+	logFatal(err)
 
   router := mux.NewRouter()
-  router.HandleFunc("/books", getBooks).Methods("GET")
-  router.HandleFunc("/books/{id}", getBook).Methods("GET")
+  controller := controllers.Controller{}
+  router.HandleFunc("/books", controller.getBooks(db)).Methods("GET")
+  /*router.HandleFunc("/books/{id}", getBook(db)).Methods("GET")
   router.HandleFunc("/books", addBook).Methods("POST")
   router.HandleFunc("/books", updateBook).Methods("PUT")
   router.HandleFunc("/books/{id}", removeBook).Methods("DELETE")
-
-  log.Fatal(http.ListenAndServe(":8000", router))
-}
-
-func getBooks(w http.ResponseWriter, r *http.Request) {
-  log.Println("Get all books")
-}
-
-func getBook(w http.ResponseWriter, r *http.Request) {
-  log.Println("Get book")
-}
-
-func addBook(w http.ResponseWriter, r *http.Request) {
-  log.Println("Add one book")
-}
-
-func updateBook(w http.ResponseWriter, r *http.Request) {
-  log.Println("update book")
-}
-
-func removeBook(w http.ResponseWriter, r *http.Request) {
-  log.Println("remove book")
+*/
+  logFatal(http.ListenAndServe(":8000", router))
 }
