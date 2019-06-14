5c52e75a0f1750566826d89b5e51cf4bb48248a5
JamesACM <jaimecanalesmarin@gmail.com>
2019-06-14 11:49:50 -0400

    Update book.go

--- controllers/book.go
+++ controllers/book.go
@@ -1,12 +1,14 @@
 package controllers
 
 import (
-	"encoding/json"
+  "encoding/json"
   "log"
   "net/http"
-  //"strconv"
+  "fmt"
+  "github.com/gorilla/mux"
+  "strconv"
   "github.com/JamesAndresCM/book_api_go/models"
-	"github.com/JamesAndresCM/book_api_go/driver"
+  "github.com/JamesAndresCM/book_api_go/driver"
 )
 
 type Controller struct{}
@@ -20,49 +22,91 @@ func logFatal(err error){
 }
 
 func (c Controller) GetBooks(db *driver.DB) http.HandlerFunc {
-		return func(w http.ResponseWriter, r *http.Request) {
-			var book models.Book
-			books = []models.Book{}
-			rows, err := db.SQL.Query("SELECT *  FROM books")
-			logFatal(err)
-		
-			defer rows.Close()
-			for rows.Next(){
-			err := rows.Scan(&book.ID, &book.Title, &book.Author, &book.Year)
-			logFatal(err)
-			books = append(books, book)
-			}
-			json.NewEncoder(w).Encode(books)
-		}
+  return func(w http.ResponseWriter, r *http.Request) {
+    var book models.Book
+    books = []models.Book{}
+    rows, err := db.SQL.Query("SELECT *  FROM books")
+    logFatal(err)
+
+    defer rows.Close()
+    for rows.Next(){
+      err := rows.Scan(&book.ID, &book.Title, &book.Author, &book.Year)
+      logFatal(err)
+      books = append(books, book)
+    }
+    fmt.Println(books)
+    json.NewEncoder(w).Encode(books)
+  }
 }
 
-  
-  
-  
-  /*
-  func getBook(db *driver.DB) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-	  var book models.Book
-	  params := mux.Vars(r)
-	  bookid, err := strconv.Atoi(params["id"])
-	  logFatal(err)
-	  rows := db.SQL.QueryRow("SELECT * FROM books where id=$1", bookid)
-	  error := rows.Scan(&book.ID, &book.Title, &book.Author, &book.Year)
-	  logFatal(error)
-	  json.NewEncoder(w).Encode(book)
-	}
+func (c Controller) GetBook(db *driver.DB) http.HandlerFunc {
+  return func(w http.ResponseWriter, r * http.Request) {
+    var book models.Book
+    data_not_found := make(map[string]string)
+    params := mux.Vars(r)
+    bookid, err := strconv.Atoi(params["id"])
+    logFatal(err)
+    rows := db.SQL.QueryRow("SELECT * FROM books where id=$1", bookid)
+    error := rows.Scan(&book.ID, &book.Title, &book.Author, &book.Year)
+    if error == nil {
+      json.NewEncoder(w).Encode(book)
+    } else {
+      data_not_found["error"] = "book not found"
+      json.NewEncoder(w).Encode(data_not_found)
+    }
   }
-  
-  func addBook(w http.ResponseWriter, r *http.Request) {
-	log.Println("Add one book")
+}
+
+
+func (c Controller) DestroyBook(db *driver.DB) http.HandlerFunc {
+  return func(w http.ResponseWriter, r * http.Request) {
+    var book models.Book
+    params := mux.Vars(r)
+    bookid, err := strconv.Atoi(params["id"])
+    logFatal(err)
+    rows := db.SQL.QueryRow("SELECT * FROM books where id=$1", bookid)
+    error := rows.Scan(&book.ID, &book.Title, &book.Author, &book.Year)
+    if error == nil {
+      db.SQL.QueryRow("DELETE FROM books where id=$1", bookid)
+      json.NewEncoder(w).Encode("book destroyed")
+    } else {
+      json.NewEncoder(w).Encode("book not found")
+    }
+//    error := rows.Scan()
+    //logFatal(err)
   }
-  
-  func updateBook(w http.ResponseWriter, r *http.Request) {
-	log.Println("update book")
+}
+/*
+posible callback id
+func getID(db *driver.DB, id int) {
+  var book models.Book
+  rows := db.SQL.QueryRow("SELECT * FROM books where id=$1", bookid)
+  error := rows.Scan(&book.ID, &book.Title, &book.Author, &book.Year)
+
+}
+/*
+func getBook(db *driver.DB) func(w http.ResponseWriter, r *http.Request) {
+  return func(w http.ResponseWriter, r *http.Request) {
+    var book models.Book
+    params := mux.Vars(r)
+    bookid, err := strconv.Atoi(params["id"])
+    logFatal(err)
+    rows := db.SQL.QueryRow("SELECT * FROM books where id=$1", bookid)
+    error := rows.Scan(&book.ID, &book.Title, &book.Author, &book.Year)
+    logFatal(error)
+    json.NewEncoder(w).Encode(book)
   }
-  
-  func removeBook(w http.ResponseWriter, r *http.Request) {
-	log.Println("remove book")
-	}
-	*/
-  
+}
+
+func addBook(w http.ResponseWriter, r *http.Request) {
+  log.Println("Add one book")
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+  log.Println("update book")
+}
+
+func removeBook(w http.ResponseWriter, r *http.Request) {
+  log.Println("remove book")
+}
+*/
